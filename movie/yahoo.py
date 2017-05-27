43d9ca2802e1032995a01ef0bc80799d25805e92
Phate334 <phate334@gmail.com>
2017-05-28 02:51:35 +0800

    add parser on theater page.

--- movie/yahoo.py
+++ movie/yahoo.py
@@ -31,9 +31,7 @@ def get_areas():
 
         area_id: Parameter for /theater_list.html .
     """
-    res = requests.get(HOST + '/theater_list.html')
-    res.raise_for_status()
-    soup = BeautifulSoup(res.text, "html.parser")
+    soup = get_soup(HOST + '/theater_list.html')
     return [(opt['value'], opt.get_text()) for opt in soup.find(id='area').find_all('option')][1:]
 
 def area_theater_parser(area_id):
@@ -46,16 +44,37 @@ def area_theater_parser(area_id):
     for g in soup.find_all(class_='group'):
         for theater in g.find('tbody').find_all('tr'):
             td = theater.find_all('td')
-            name = td[0].get_text()
-            url = td[0].find('a')['href'].split('*')[1]
-            t_id = re.search(r'\d+', url).group()
-            address = td[1].contents[0]
-            tel = td[1].contents[1].get_text()
-            yield {
-                'id': t_id,
-                'name': name,
-                'area_id': area_id,
-                'address': address,
-                'url': url,
-                'tel': tel
-                }
+            theater = {}
+            theater["name"] = td[0].get_text()
+            theater["area_id"] = area_id
+            theater["url"] = td[0].find('a')['href'].split('*')[1]
+            theater["id"] = re.search(r'\d+', theater["url"]).group()
+            theater["address"] = td[1].contents[0]
+            theater["tel"] = td[1].contents[1].get_text()
+            yield theater
+
+def get_theater_time(theater_id):
+    """ time data on the theater page.
+    """
+    soup = get_soup(HOST + "/theater_result.html/id=" + str(theater_id))
+    movie_div_list = soup.find_all("div", {"class":["row", "row_last"]})
+    result = []
+    for div in movie_div_list:
+        movie = {}
+        movie["url"] = div.find("h4").a["href"].split('*')[1]
+        movie["id"] = re.search(r'\d+', movie["url"]).group()
+        movie["title_tw"] = div.find("h4").text
+        movie["mvtype"] = []
+        for img in div.find("span", class_="mvtype").find_all("img"):
+            movie["mvtype"].append(
+                re.search('icon_(.*).gif', img["src"]).group(1))
+        movie["time"] = []
+        for span in div.find_all("span", class_="tmt"):
+            movie["time"].append(span.text)
+        result.append(movie)
+    return result
+
+def get_soup(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    return BeautifulSoup(res.text, "html.parser")
